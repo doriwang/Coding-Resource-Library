2a14ba545890d9212ad30ca37309987c31b1ea71
Yajiao Wang <dori.wyj@gmail.com>
2020-07-08 15:22:38 -0600

    wired up html view routes, inserted note and todolist page and functionality

--- schema.sql
+++ schema.sql
@@ -1,4 +1,2 @@
--- Drops the blogger if it exists currently --
 DROP DATABASE IF EXISTS code_resource_library;
--- Creates the "blogger" database --
 CREATE DATABASE code_resource_library;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
+var noteApi = require("./routes/note-api-routes");
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -20,13 +21,15 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
+app.use("/api", noteApi);
 
 // Static directory
 app.use(express.static("public"));
 
 // routes 
-require("./routes/api-routes.js")(app);
+// require("./routes/api-routes.js")(app);
 require("./routes/view-routes.js")(app);
+require("./routes/todo-api-routes")(app);
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync({
