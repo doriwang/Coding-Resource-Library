da85255299274ec92bf341efa03e428681e302a8
Jonathan Tousley <Jbtousley@gmail.com>
2020-07-08 18:51:03 -0600

    fixed index.js bug

--- models/index.js
+++ models/index.js
@@ -4,13 +4,9 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-<<<<<<< HEAD
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
-=======
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
->>>>>>> 077cb77cc89be8760640e26bcc9900afa0c11957
+
 const db = {};
 
 let sequelize;
